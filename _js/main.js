7a1519459ebfa64354523fd8aebdb8b598f3d55d
Valjean1 <nicolas53301@gmail.com>
2021-05-24 23:12:03 -0300

    Clone da pagina inicial do netflix, projeto da DIO

---
+++ _js/main.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+function botaoAssistir(elemento){
+    window.open("https://www.youtube.com/watch?v=xUntodg-vXY")
+}
+
+/* function botaoInfo(elemento){
+    Não consegui criar um popup :c  
+}
+*/
